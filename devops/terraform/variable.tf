8a4e7258b6beb630f444f4f7c2f47819ad1e1704
raushankumarrr88 <111422679+raushankumarrr88@users.noreply.github.com>
2024-10-28 15:23:55 +0530

    Create variable.tf

---
+++ devops/terraform/variable.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "The AWS region"
+  type        = string
+}
+
+variable "ecr_repository_name" {
+  description = "The name of the ECR repository"
+  type        = string
+}
