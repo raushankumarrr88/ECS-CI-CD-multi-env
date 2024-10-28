68a0bd4ff6235aff6f01185cbdd104345870b576
raushankumarrr88 <111422679+raushankumarrr88@users.noreply.github.com>
2024-10-28 16:48:51 +0530

    Update main.tf

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -1,7 +1,20 @@
-provider "aws" {
-  region = var.aws_region
+# Specify the required Terraform version
+terraform {
+  required_version = ">= 1.0.0"
+
+  backend "s3" {
+    bucket = "ecs-deployment-terraform-statefile"  # Replace with your S3 bucket name
+    key    = "terraform/state"        # Path to the state file in the bucket
+    region = var.aws_region           # Use the variable for region
+  }
 }
 
+# Provider configuration
+provider "aws" {
+  region = var.aws_region  # AWS region from variable
+}
+
+# Resource to create an ECR repository
 resource "aws_ecr_repository" "my_repository" {
-  name = var.ecr_repository_name
+  name = var.ecr_repository_name  # Name of the ECR repository
 }
