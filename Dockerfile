b854d515f67617f116e8723f5a8cfc5412aa3a87
raushanchaudhary840.rc <raushanchaudhary840.rc@gmail.com>
2024-10-29 11:58:58 +0530

    added task defination

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
-EXPOSE 5000
+EXPOSE 80
 # ds
 CMD ["python", "app.py"]
---
+++ devops/terraform/ecs_task_defination.tf
@@ -0,0 +1,37 @@
+# ECS Task Definition for Staging
+resource "aws_ecs_task_definition" "staging_task" {
+  family                   = "staging-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"  # Adjust based on application requirements
+  memory                   = "512"  # Adjust based on application requirements
+
+  container_definitions = jsonencode([{
+    name      = "staging-container"
+    image     = "${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com/${var.ecr_repository_name}:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+    }]
+  }])
+}
+
+# ECS Task Definition for Production
+resource "aws_ecs_task_definition" "production_task" {
+  family                   = "production-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"  # Adjust based on application requirements
+  memory                   = "512"  # Adjust based on application requirements
+
+  container_definitions = jsonencode([{
+    name      = "production-container"
+    image     = "${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com/${var.ecr_repository_name}:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+    }]
+  }])
+}
