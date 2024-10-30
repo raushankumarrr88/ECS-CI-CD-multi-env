1d8b7a17717d67544e1159e35dea458cbc5c8350
raushanchaudhary840.rc <raushanchaudhary840.rc@gmail.com>
2024-10-30 23:14:54 +0530

    added load balacer

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/demo')
+@app.route('/')
 def hello():
     return "Hello, World!"
+
+@app.route('/demo')
+def hello():
+    return "Hello, World! This is from demo endpoint"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
