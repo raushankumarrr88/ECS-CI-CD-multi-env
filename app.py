6c5fc8e94621f00fa517a63fe5fe72b94625a738
raushanchaudhary840.rc <raushanchaudhary840.rc@gmail.com>
2024-11-06 11:31:10 +0530

    added load balacer

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, jsonify
 
+app = Flask(__name__)
+
 @app.route('/micros1/')
 def hello():
     return "Hello, World! from route"
 
+@app.route('/micros1/demo')
+def demo():
+    return "Hello, World! This is from demo endpoint"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
